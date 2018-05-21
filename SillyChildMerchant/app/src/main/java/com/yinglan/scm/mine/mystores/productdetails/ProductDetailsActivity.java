817d93bf21851c12a287d6e184c884e83e449163
921668753 <921668753@qq.com>
2018-05-21 11:41:49 +0800

    fix BUG

---
+++ SillyChildMerchant/app/src/main/java/com/yinglan/scm/mine/mystores/productdetails/ProductDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.yinglan.scm.mine.mystores.productdetails;
+
+import com.common.cklibrary.common.BaseActivity;
+import com.yinglan.scm.R;
+
+/**
+ * 产品详情
+ */
+public class ProductDetailsActivity extends BaseActivity {
+
+    @Override
+    public void setRootView() {
+        setContentView(R.layout.activity_productdetails);
+    }
+}
---
+++ SillyChildMerchant/app/src/main/java/com/yinglan/scm/mine/mystores/releasegoods/ReleaseGoodsActivity.java
@@ -0,0 +1,14 @@
+package com.yinglan.scm.mine.mystores.releasegoods;
+
+import com.common.cklibrary.common.BaseActivity;
+import com.yinglan.scm.R;
+
+/**
+ * 发布商品
+ */
+public class ReleaseGoodsActivity extends BaseActivity {
+    @Override
+    public void setRootView() {
+        setContentView(R.layout.activity_releasegoods);
+    }
+}
