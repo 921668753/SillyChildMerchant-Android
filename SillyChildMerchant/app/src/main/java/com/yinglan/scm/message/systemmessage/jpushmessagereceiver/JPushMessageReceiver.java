c74e56bd543d6d5494c806a28937fd71ebac456f
921668753 <921668753@qq.com>
2018-07-03 11:33:04 +0800

    fix bug

--- SillyChildMerchant/app/src/main/java/com/yinglan/scm/message/systemmessage/jpushmessagereceiver/JPushMessageReceiver.java
+++ SillyChildMerchant/app/src/main/java/com/yinglan/scm/message/systemmessage/jpushmessagereceiver/JPushMessageReceiver.java
@@ -35,20 +35,20 @@ public class JPushMessageReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Bundle bundle = intent.getExtras();
-        Log.d(TAG, "[MyReceiver] onReceive - " + intent.getAction() + ", extras: " + printBundle(bundle));
+        Log.d(TAG, "[JPushMessageReceiver] onReceive - " + intent.getAction() + ", extras: " + printBundle(bundle));
 
         if (JPushInterface.ACTION_REGISTRATION_ID.equals(intent.getAction())) {
             String regId = bundle.getString(JPushInterface.EXTRA_REGISTRATION_ID);
-            Log.d(TAG, "[MyReceiver] 接收Registration Id : " + regId);
+            Log.d(TAG, "[JPushMessageReceiver] 接收Registration Id : " + regId);
             //send the Registration Id to your server...
 
         } else if (JPushInterface.ACTION_MESSAGE_RECEIVED.equals(intent.getAction())) {
-            Log.d(TAG, "[MyReceiver] 接收到推送下来的自定义消息: " + bundle.getString(JPushInterface.EXTRA_MESSAGE));
+            Log.d(TAG, "[JPushMessageReceiver] 接收到推送下来的自定义消息: " + bundle.getString(JPushInterface.EXTRA_MESSAGE));
             processCustomMessage(context, bundle);
         } else if (JPushInterface.ACTION_NOTIFICATION_RECEIVED.equals(intent.getAction())) {
-            Log.d(TAG, "[MyReceiver] 接收到推送下来的通知");
+            Log.d(TAG, "[JPushMessageReceiver] 接收到推送下来的通知");
             int notifactionId = bundle.getInt(JPushInterface.EXTRA_NOTIFICATION_ID);
-            Log.d(TAG, "[MyReceiver] 接收到推送下来的通知的ID: " + notifactionId);
+            Log.d(TAG, "[JPushMessageReceiver] 接收到推送下来的通知的ID: " + notifactionId);
             Intent intent1 = new Intent();
           //  intent1.setAction(MESSAGE_RECEIVED_ACTION);
             intent1.setAction(MainServiceAction);
@@ -58,7 +58,7 @@ public class JPushMessageReceiver extends BroadcastReceiver {
             //  intent.setAction(BROADCAST_ACTION);
             //发送广播
         } else if (JPushInterface.ACTION_NOTIFICATION_OPENED.equals(intent.getAction())) {
-            Log.d(TAG, "[MyReceiver] 用户点击打开了通知");
+            Log.d(TAG, "[JPushMessageReceiver] 用户点击打开了通知");
             //打开自定义的Activity
             Intent news = new Intent(context, MainActivity.class);
             news.putExtras(bundle);
@@ -71,14 +71,14 @@ public class JPushMessageReceiver extends BroadcastReceiver {
             news.putExtra("havemsg", false);
             context.sendBroadcast(news);
         } else if (JPushInterface.ACTION_RICHPUSH_CALLBACK.equals(intent.getAction())) {
-            Log.d(TAG, "[MyReceiver] 用户收到到RICH PUSH CALLBACK: " + bundle.getString(JPushInterface.EXTRA_EXTRA));
+            Log.d(TAG, "[JPushMessageReceiver] 用户收到到RICH PUSH CALLBACK: " + bundle.getString(JPushInterface.EXTRA_EXTRA));
             //在这里根据 JPushInterface.EXTRA_EXTRA 的内容处理代码，比如打开新的Activity， 打开一个网页等..
 
         } else if (JPushInterface.ACTION_CONNECTION_CHANGE.equals(intent.getAction())) {
             boolean connected = intent.getBooleanExtra(JPushInterface.EXTRA_CONNECTION_CHANGE, false);
-            Log.w(TAG, "[MyReceiver]" + intent.getAction() + " connected state change to " + connected);
+            Log.w(TAG, "[JPushMessageReceiver]" + intent.getAction() + " connected state change to " + connected);
         } else {
-            Log.d(TAG, "[MyReceiver] Unhandled intent - " + intent.getAction());
+            Log.d(TAG, "[JPushMessageReceiver] Unhandled intent - " + intent.getAction());
         }
     }
 
