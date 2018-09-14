97eda98c44de8492b42bd7e44bebfefcdec3f28f
921668753 <921668753@qq.com>
2018-09-14 13:23:41 +0800

    fix BUG

--- SillyChildMerchant/app/src/main/java/com/yinglan/scm/constant/URLConstants.java
+++ SillyChildMerchant/app/src/main/java/com/yinglan/scm/constant/URLConstants.java
@@ -17,9 +17,8 @@ public class URLConstants {
     /**
      * 测试服务器地址URL
      */
-    public static String SERVERURL = "http://192.168.1.247:8080/";
-//    public static String SERVERURL = "http://store.api.shahaizhi.com/";
-
+//    public static String SERVERURL = "http://192.168.1.247:8080/";
+    public static String SERVERURL = "http://store.api.shahaizhi.com/";
     public static String SERVERURL1 = "http://www.shahaizhi.tech/";
 
     /**
